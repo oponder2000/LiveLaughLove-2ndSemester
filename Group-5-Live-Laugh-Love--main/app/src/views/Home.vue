b60ad9febc1e77516ffdb1826a6287367a3b7f37
JeremyJarms <105084393+JeremyJarms@users.noreply.github.com>
2023-09-17 01:17:05 -0500

    Home.vue Update
    Load Game button now toggles a div with a drag and drop input field.

--- Group-5-Live-Laugh-Love--main/app/src/views/Home.vue
+++ Group-5-Live-Laugh-Love--main/app/src/views/Home.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+    const loadButton = document.getElementById("load_button");
+    //https://stackoverflow.com/questions/19074171/how-to-toggle-a-divs-visibility-by-using-a-button-click
+    function toggleDiv(id: string) {
+        let div = document.getElementById(id);
+        if(div != null) {
+            if(div.style.display == "none") {
+                div.style.display = "block";
+            } else {
+                div.style.display = "none";
+            }
+        }
+    }
+</script>
+
 <template>
     <section class="card bg-base-200 py-5 mx-auto w-3/4">
         <div class="card-body w-3/4 mx-auto">
@@ -7,7 +22,11 @@
                 story games. Try out our sample game, or get started on writing your own.
             </h2>
             <RouterLink to="game" class="btn">New Game</RouterLink>
-            <RouterLink to="/" class="btn">Load Game</RouterLink>
+            <button class="btn" id="load_button" v-on:click="toggleDiv('load_div');">Load Game</button>
+            <div id="load_div" style="display: none;">
+                <!--<p>Insert Game File</p>-->
+                <input type="file" accept=".txt">
+            </div>
             <RouterLink to="editor" class="btn">Create Game</RouterLink>
         </div>
         <div class="flex  justify-center mx-auto rounded bg-cyan-600  text-center border-black shadow py-1 w-40">
