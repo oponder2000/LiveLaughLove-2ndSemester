0e914c986ea6878e430b892c744b4d1ac7f84aff
JeremyJarms <105084393+JeremyJarms@users.noreply.github.com>
2023-10-24 11:38:23 -0500

    Transitioning a block of code into a component.

--- Group-5-Live-Laugh-Love--main/app/src/views/Editor.vue
+++ Group-5-Live-Laugh-Love--main/app/src/views/Editor.vue
@@ -1,26 +1,13 @@
 <script setup lang="ts">
     import { ref, onMounted } from 'vue';
-    import { useDraftStore } from "../stores/editorStore";
-    import emptyStory from "../models/templateData";
-
-    const store = useDraftStore();
+    import MakeNewAdventure from '../components/MakeNewAdventure.vue'
 </script>
 <template>
-    
-    <div class="card bg-base-200 py-5 mx-auto w-3/4">
-        <div class="card-body w-3/4 mx-auto">
-            <div>
-                <label for="tname" class="block">Title:</label>
-                <input type="text" id= "tname" name="title" placeholder="title">
-                <label for="dname" class="block">Description:</label>
-                <input type="text" id= "dname" name="description" placeholder="description">
-            </div>
-            <div class="">
-                <button class="btn btn-primary block" @click=";store.initNewDraft(false)">
-                    Create an Adventure
-                </button>
-                <router-link to="/" class="btn">Exit</router-link>
-            </div>    
-        </div>  
+    <make-new-adventure></make-new-adventure>
+    <div>
+        <h1>Editor</h1>
+        <button class="btn">Story</button>
+        
+
     </div>
 </template>
