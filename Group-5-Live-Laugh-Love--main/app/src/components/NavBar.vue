9b6d0ebd36c60b9a488d8b04bc951fa4883d17d6
OliviaCantrell <oliviacantrell@my.unt.edu>
2023-11-12 23:27:52 -0600

    Update NavBar.vue
    Beginning Merge Process

--- Group-5-Live-Laugh-Love--main/app/src/components/NavBar.vue
+++ Group-5-Live-Laugh-Love--main/app/src/components/NavBar.vue
@@ -1,44 +1,34 @@
 <script setup lang="ts">
-import { getAuth, signOut } from '@firebase/auth';
-
-const auth = getAuth();
-
-
-function signOutUser() {
-    signOut(auth)
-        .then(() => {
-            console.log('Signed Out');
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-}
+import { defineProps } from 'vue';
 
 const props = defineProps({
   selectedFont: String, // Receive the selected font as a prop
+  selectedColor: String, // Receive the selected color as a prop
 });
 </script>
 
 <template>
-    <div class="py-2">
-        <div class="container flex justify-between items-center mx-auto">
-            <router-link to="/" class="text-3xl  font-bold">
-                Text Based Adventure
-            </router-link>
-            <!-- This could be made into a collapsible menu -->
-            <div class="flex justify-start gap-2 w-1/2">
-                <RouterLink to="tts" class="btn">
-                    Text-To-Speech
-                </RouterLink>
-                <button class="btn" :style="{ fontFamily: selectedFont }">
-                    Font Size
-                </button>
-                <button class="btn" :style="{ fontFamily: selectedFont }">
-                    Epileptic Effects
-                </button>
-                <button class="btn" :style="{ fontFamily: selectedFont }" data-toggle-theme="dark,light" data-act-class="ACTIVECLASS">
-                    Dark Mode Toggle
-                </button>
+  <div class="py-2">
+    <div class="container flex justify-between items-center mx-auto">
+      <router-link to="/" class="text-3xl font-bold" :style="{ fontFamily: selectedFont, color: selectedColor }">
+        Text Based Adventure
+      </router-link>
+      <!-- This could be made into a collapsible menu -->
+      <div class="flex justify-start gap-2 w-1/2">
+        <RouterLink to="tts" class="btn" :style="{ fontFamily: selectedFont, color: selectedColor }">
+          Text-To-Speech
+        </RouterLink>
+        <button class="btn" :style="{ fontFamily: selectedFont, color: selectedColor }">Font Size</button>
+        <button
+          class="btn"
+          data-toggle-theme="dark,light"
+          data-act-class="ACTIVECLASS"
+          :style="{ fontFamily: selectedFont, color: selectedColor }"
+        >
+          Dark Mode Toggle
+        </button>
+
+
                 <label for="data-choose-theme">High Contrast Theme: </label>
                 <select data-choose-theme>
                 <option value="hc1">Orange-Blue</option>
@@ -47,11 +37,10 @@ const props = defineProps({
                 <option value="hc4">Tan-Turquoise</option>
                 </select>
             </div>
-            
             <nav class="flex justify-end gap-2 w-1/4">
-                <RouterLink to="/" class="btn"> Home </RouterLink>
-            </nav>
-        </div>
+        <RouterLink to="/" class="btn" :style="{ fontFamily: selectedFont, color: selectedColor }">Home</RouterLink>
+      </nav>
     </div>
+  </div>
 </template>
 
