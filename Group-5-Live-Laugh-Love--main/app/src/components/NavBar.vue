dc6cc0a0f599c5620180ba40ae17e31f6af57754
oponder2000 <oponder2000@gmail.com>
2023-11-12 18:11:41 -0600

    fixed a bug where the navbar was changing color but no longer font, both change now

--- Group-5-Live-Laugh-Love--main/app/src/components/NavBar.vue
+++ Group-5-Live-Laugh-Love--main/app/src/components/NavBar.vue
@@ -15,22 +15,22 @@ const props = defineProps({
       </router-link>
       <!-- This could be made into a collapsible menu -->
       <div class="flex justify-start gap-2 w-1/2">
-        <RouterLink to="tts" class="btn" :style="{ color: selectedColor }">
+        <RouterLink to="tts" class="btn" :style="{ fontFamily: selectedFont, color: selectedColor }">
           Text-To-Speech
         </RouterLink>
-        <button class="btn" :style="{ color: selectedColor }">Font Size</button>
+        <button class="btn" :style="{ fontFamily: selectedFont, color: selectedColor }">Font Size</button>
         <button
           class="btn"
           data-toggle-theme="dark,light"
           data-act-class="ACTIVECLASS"
-          :style="{ color: selectedColor }"
+          :style="{ fontFamily: selectedFont, color: selectedColor }"
         >
           Dark Mode Toggle
         </button>
       </div>
 
       <nav class="flex justify-end gap-2 w-1/4">
-        <RouterLink to="/" class="btn" :style="{ color: selectedColor }">Home</RouterLink>
+        <RouterLink to="/" class="btn" :style="{ fontFamily: selectedFont, color: selectedColor }">Home</RouterLink>
       </nav>
     </div>
   </div>
