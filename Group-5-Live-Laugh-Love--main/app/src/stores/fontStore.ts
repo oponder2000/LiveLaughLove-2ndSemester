49dc56949f062d07dca80503b23e5080a2f654b2
humzakh <humzakh@pm.me>
2023-11-16 17:33:49 -0600

    minor text fix

--- Group-5-Live-Laugh-Love--main/app/src/stores/fontStore.ts
+++ Group-5-Live-Laugh-Love--main/app/src/stores/fontStore.ts
@@ -3,9 +3,9 @@ import { defineStore } from "pinia";
 
 export const useFontStore = defineStore('font', () => {
     const fonts = ["Arial", "Times New Roman", "Verdana"];
-    const colors = ["white", "red", "blue"]
+    const colors = [null, "white", "black", "red", "blue"]
     const selectedFont = ref("Arial");
-    const selectedColor = ref("white");
+    const selectedColor = ref(null);
 
     return {
         fonts,
